769ed86748440a35b7dc6c92ac319979e9f44cc4
ravijat2026 <ravijat2026@gmail.com>
2024-11-06 09:51:22 +0530

    frontend url updated

--- backend/constants.py
+++ backend/constants.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-SERVER_URL = 'localhost'
+SERVER_URL = 'https://ai-calculator-nine.vercel.app/'
 PORT = 8000
 ENV = 'dev'
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
